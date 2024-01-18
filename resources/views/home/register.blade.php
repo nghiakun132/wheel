fa34ea67de2150b367c648927d95191c7427b43f
bui.nghia <bui.nghia@rivercrane.com.vn>
2024-01-18 09:59:27 +0700

    fix small

--- resources/views/home/register.blade.php
+++ resources/views/home/register.blade.php
@@ -2,7 +2,7 @@
     <div class="row">
         <div class="col-md-12">
             <div class="logo">
-                <img src="{{ asset('image/luckydraw-images-02.png') }}" width="100%" height="100%" />
+                <img src="{{ asset('image/luckydraw-images-02.png') }}" width="70%"  />
             </div>
 
             <div class="">
