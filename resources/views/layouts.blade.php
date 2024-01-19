49836af51cd2c361f1d7e6c3f9f20cfccc63a312
bui.nghia <bui.nghia@rivercrane.com.vn>
2024-01-19 07:59:33 +0700

    sync

--- resources/views/layouts.blade.php
+++ resources/views/layouts.blade.php
@@ -5,6 +5,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    @if(config('app.https') == true)
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    @endif
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="shortcut icon" href="img/icons/icon-48x48.png" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
